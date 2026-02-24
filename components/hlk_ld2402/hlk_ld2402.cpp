aa09f5a31449414490ddeb196b8e8d55b932cccd
sisheng36 <45661212+sisheng36@users.noreply.github.com>
2026-02-24 22:55:19 +0800

    Update hlk_ld2402.cpp

--- components/hlk_ld2402/hlk_ld2402.cpp
+++ components/hlk_ld2402/hlk_ld2402.cpp
@@ -1,7 +1,6 @@
 #include "hlk_ld2402.h"
 #include "esphome.h"
 #include "esphome/core/log.h"
-#include "esp_system.h"
 
 namespace esphome {
 namespace hlk_ld2402 {
@@ -244,7 +243,6 @@ void HLKLD2402Component::loop() {
         sprintf(hex_buf + (i*3), "%02X ", last_bytes[i]);
         sprintf(ascii_buf + i, "%c", (last_bytes[i] >= 32 && last_bytes[i] < 127) ? last_bytes[i] : '.');
       }
-      ESP_LOGI(TAG, "Free heap now: %u", esp_get_free_heap_size());
     }
     byte_count = 0;
     last_status_time = millis();
